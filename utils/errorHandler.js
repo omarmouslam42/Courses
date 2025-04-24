7a7411c632fa4c881658bbe1ffd501fa3933f15c
omarmouslam42 <abomoslamomar@gmail.com>
2025-04-24 23:34:29 +0200

    change mongourl

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -6,6 +6,6 @@ const errorHandler = (err, req, res, next) => {
       stack: process.env.NODE_ENV === 'production' ? null : err.stack,
     });
   };
-  
+   
   module.exports = { errorHandler };
-  
+   
